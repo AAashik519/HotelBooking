97f694de2b11ab59d912ad94c28fe39ce6f04fe8
AAashik519 <64531121+AAashik519@users.noreply.github.com>
2022-11-04 17:11:14 +0600

    featured property

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,6 +3,8 @@ import './home.css'
 import Featured from '../../components/Featured/Featured'
 import Header from '../../components/Header/Header'
 import Nabvar from '../../components/Navbar/Nabvar'
+import Property from '../../components/Property/Property'
+import FeaturedProperties from '../../components/FeaturedProperties/FeaturedProperties'
 
 const Home = () => {
   return (
@@ -12,7 +14,9 @@ const Home = () => {
       <div className='homeContainer'>
         <Featured />
         <h1 className='homeTitle'> Browse by property type</h1>
-        
+        <Property />
+        <h1 className='homeTitle'> Gomes guests love</h1>
+          <FeaturedProperties />
 
       </div>
     </div>
